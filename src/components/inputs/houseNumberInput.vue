86c4c9f7ceca7999c2c36ad491e45966df92131e
oleniev_vova <oleniev9@gmail.com>
2023-02-03 15:44:35 +0200

    second commit - licence plate input

---
+++ src/components/inputs/houseNumberInput.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <label for="zipcode">Enter your house number</label>
+    <input id="zipcode" @change="zip" maxlength="6" type="text" v-model="houseNumber">
+    <div v-if="zipCode.length === 6">{{this.zipCodeValidator}}</div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue } from 'vue-class-component';
+
+export default class HouseNumberInput extends Vue {
+  houseNumber = ''
+
+  zip(event: any): void {
+    if (/^\d+$/.test(this.zipCode)) {
+      this.zipCodeValidator = 'zip code is valid';
+      this.$emit('zipCode', this.zipCode);
+    } else {
+      this.zipCodeValidator = ' zip code is Invalid';
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/inputs/zipCodeInput.vue
@@ -0,0 +1,30 @@
+<template>
+<div>
+  <label for="zipcode">Enter your zip code</label>
+  <input id="zipcode" @change="zip" maxlength="6" type="text" v-model="zipCode">
+  <div v-if="zipCode.length === 6">{{this.zipCodeValidator}}</div>
+</div>
+</template>
+
+<script lang="ts">
+import { Vue } from 'vue-class-component';
+
+export default class ZipCodeInput extends Vue {
+  zipCode = ''
+
+  zipCodeValidator = ''
+
+  zip(event: any): void {
+    if (/^\d+$/.test(this.zipCode)) {
+      this.zipCodeValidator = 'zip code is valid';
+      this.$emit('zipCode', this.zipCode);
+    } else {
+      this.zipCodeValidator = ' zip code is Invalid';
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
